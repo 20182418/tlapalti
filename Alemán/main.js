e2cabb9caef7049475f87d2f83a19548b865f273
Noguerón Méndez José Antonio <89807733+Antonio297@users.noreply.github.com>
2022-08-11 19:55:56 -0500

    Add files via upload

---
+++ Alemán/main.js
@@ -0,0 +1,25 @@
+const typed= new Typed('.typed',{
+    strings:[
+        '<i class="place">INNOVACION</i>',
+        '<i class="place">INVESTIGACION</i>',
+        '<i class="place">CIENCIAS</i>',
+        '<i class="place">ARTE</i>',
+        '<i class="place">CREATIVIDAD</i>',
+        '<i class="place">ORIGINALIDAD</i>',
+        '<i class="place">HUMANIDADES</i>',
+        '<i class="place">PROGRESO</i>',
+        '<i class="place">LIBERTAD</i>',
+      
+    ],
+    stringsElement:'#cadenas-texto',
+    typeSpeed: 80,
+    startDelay:100,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    showCursor: true,
+    cursorChar:'|',
+    contentType:'html'
+});
---
+++ Alemán/suggestions.js
@@ -0,0 +1,31 @@
+let suggestions = [
+    
+     "La historia del color",
+     "Captura de periodos pendulares",
+     "Nosotros",
+     "Nuestra Historia",
+     "Contacto",
+     "Documentos",
+     "General",
+     "Investigaciones",
+     "Videos",
+     "Artículos",
+     "Infografías",
+     "Noticia del día",
+     "Novedades",
+     "Lo más Reciente",
+     "Catálogo",
+     "Investigadores",
+     "General",
+     "Áreas",
+     "Tlapalti",
+     "Facebook",
+     "Twitter",
+     "Instagram",
+     "Youtube",
+     "Correo",
+     "José Antonio",
+     "Francisco Anthony",
+    
+    
+];
