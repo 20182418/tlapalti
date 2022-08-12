322ebe6450a358371d735b571174c29bde364c97
Noguerón Méndez José Antonio <89807733+Antonio297@users.noreply.github.com>
2022-08-11 22:26:57 -0500

    Update main.js

--- Francés/main.js
+++ Francés/main.js
@@ -1,14 +1,14 @@
 const typed= new Typed('.typed',{
     strings:[
-        '<i class="place">INNOVACION</i>',
-        '<i class="place">INVESTIGACION</i>',
-        '<i class="place">CIENCIAS</i>',
-        '<i class="place">ARTE</i>',
-        '<i class="place">CREATIVIDAD</i>',
-        '<i class="place">ORIGINALIDAD</i>',
-        '<i class="place">HUMANIDADES</i>',
-        '<i class="place">PROGRESO</i>',
-        '<i class="place">LIBERTAD</i>',
+        '<i class="place">INNOVATION</i>',
+        '<i class="place">RECHERCHER</i>',
+        '<i class="place">LES SCIENCES</i>',
+        '<i class="place">ART</i>',
+        '<i class="place">LA CRÉATIVITÉ</i>',
+        '<i class="place">ORIGINALITÉ</i>',
+        '<i class="place">HUMANITÉS</i>',
+        '<i class="place">PROGRÈS</i>',
+        '<i class="place">LIBERTÉ</i>',
       
     ],
     stringsElement:'#cadenas-texto',
