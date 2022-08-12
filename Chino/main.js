74e75f40f3ac8760cbcede138f0361ed9f8a9ef4
Noguerón Méndez José Antonio <89807733+Antonio297@users.noreply.github.com>
2022-08-12 11:11:39 -0500

    Update main.js

--- Chino/main.js
+++ Chino/main.js
@@ -1,14 +1,14 @@
 const typed= new Typed('.typed',{
     strings:[
-        '<i class="place">INNOVACION</i>',
-        '<i class="place">INVESTIGACION</i>',
-        '<i class="place">CIENCIAS</i>',
-        '<i class="place">ARTE</i>',
-        '<i class="place">CREATIVIDAD</i>',
-        '<i class="place">ORIGINALIDAD</i>',
-        '<i class="place">HUMANIDADES</i>',
-        '<i class="place">PROGRESO</i>',
-        '<i class="place">LIBERTAD</i>',
+        '<i class="place">創新</i>',
+        '<i class="place">研究</i>',
+        '<i class="place">科學</i>',
+        '<i class="place">藝術</i>',
+        '<i class="place">創造力</i>',
+        '<i class="place">獨創性</i>',
+        '<i class="place">人文學科</i>',
+        '<i class="place">進步</i>',
+        '<i class="place">自由</i>',
       
     ],
     stringsElement:'#cadenas-texto',
